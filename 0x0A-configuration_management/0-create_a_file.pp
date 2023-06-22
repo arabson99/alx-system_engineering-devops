163489143561068d5445829da9ca03ccc52adaf2
arabson99 <111277887+arabson99@users.noreply.github.com>
2023-06-22 20:01:34 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# The puppet manifest that creates a file school in /tmp.
-# The file School has permission 0744, belonging to the user and group www-data.
-file { '/tmp/school':
-  ensure  => present,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,8 +0,0 @@
-# The puppet manifest that kills a process named killmenow.
-
-exec { 'killing a process using pkill':
-  command  => 'pkill -9 killmenow',
-  path     => '/usr/bin:/bin',
-  onlyif   => 'pgrep killmenow',
-  provider => 'shell',
-}
