4fe42b9b67abf333d91606cb07efc4fe7e69abd8
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-07-07 22:34:17 +0100

    checked

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Install a specific version of flask (2.1.0)
-package { 'Flask':
+# Ensure pip is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => pip3,
+  require  => Package['python3-pip'],
 }
