8e6efba7970c05291e96c6f961fde9488c0de560
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-08-14 02:03:46 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,22 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+	"""Return the total number of subscribers on a given subreddit."""
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {
+		"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+	}
+	response = requests.get(url, headers=headers, allow_redirects=False)
+	if response.status_code == 404:
+		return 0
+
+	try:
+		data = response.json()
+		if "data" not in data:
+			return 0
+		results = data.get("data", {})
+		return results.get("subscribers", 0)
+
+	except ValueError:
+		return 0
+	return 0
