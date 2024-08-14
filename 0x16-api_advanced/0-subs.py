65ee5156cfbe41e085eb39963da86c5ebcea5899
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-08-14 02:05:12 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Function that queries the Reddit API and returns the number of subscribers."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-	"""Return the total number of subscribers on a given subreddit."""
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {
-		"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-	}
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	if response.status_code == 404:
-		return 0
-
-	try:
-		data = response.json()
-		if "data" not in data:
-			return 0
-		results = data.get("data", {})
-		return results.get("subscribers", 0)
-
-	except ValueError:
-		return 0
-	return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
