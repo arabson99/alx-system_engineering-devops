555de2ac66f2580d6f0558687636debcc5459f21
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-08-16 03:31:12 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,12 +14,13 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers if the subreddit is valid, otherwise 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'redquery'}
+    headers = {'user-agent': 'request'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
+		if response.status_code != 200:
+			return 0
+        data = response.json().get("data")
+        return data.get("subscribers")
     except requests.exceptions.RequestException:
         return 0
