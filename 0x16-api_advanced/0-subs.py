cf55d6e27f046a97dce61afc7b5698058f415b7a
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-08-16 02:45:58 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""A script that gets the number of subscribers for a subreddit"""
+import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    """Function that requests the number of subscribers for a subreddit"""
+
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    USER_AGENT = "com.holbertonschool.myredditscript:0.0.1 (by /u/dmaring)"
+    headers = {'User-Agent': USER_AGENT}
+    r = requests.get(URL, headers=headers)
+    if (r.status_code is not 200):
+        return(0)
+    r = r.json()
+    return(r['data']['subscribers'])
