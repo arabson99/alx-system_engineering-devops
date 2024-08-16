0d177bf732b50b4be1c2a44ed884aaa334c725b6
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-08-16 03:02:06 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,16 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit.
-    
+
     Args:
         subreddit (str): The name of the subreddit to query.
-        
+
     Returns:
-        int: The number of subscribers, or 0 if an error occurs or the subreddit is invalid.
+        int: The number of subscribers, or 0 if an error occurs or 
+        the subreddit is invalid.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
