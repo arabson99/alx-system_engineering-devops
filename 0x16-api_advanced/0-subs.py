0eff14d455713ba57380e9a95d01f21d58f253c7
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-08-16 03:00:39 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""A script that gets the number of subscribers for a subreddit"""
-import json
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Function that requests the number of subscribers for a subreddit"""
+    """Return the total number of subscribers on a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit to query.
+        
+    Returns:
+        int: The number of subscribers, or 0 if an error occurs or the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
 
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    USER_AGENT = "com.holbertonschool.myredditscript:0.0.1 (by /u/dmaring)"
-    headers = {'User-Agent': USER_AGENT}
-    r = requests.get(URL, headers=headers)
-    if (r.status_code is not 200):
-        return(0)
-    r = r.json()
-    return(r['data']['subscribers'])
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 404:
+            return 0
+        response.raise_for_status()  # Raise an exception for HTTP error responses
+        data = response.json().get("data")
+        if data is None:
+            return 0
+        return data.get("subscribers", 0)
+    except requests.RequestException:
+        return 0
